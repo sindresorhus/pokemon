3d94159f92b8322b287a84c9c43f8c15dc55fe2c
Robert James Gabriel <robert_gabriel@outlook.com>
2016-07-27 15:46:20 -0500

    Fixed to allow for 000 id number

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ test(t => {
 	t.true(m.all.length > 0);
 	t.truthy(m.random());
 	t.not(m.random(), m.random());
-	t.is(m.all[0], 'Bulbasaur');
-	t.is(m.all[1], 'Ivysaur');
+	t.is(m.all[1], 'Bulbasaur');
+	t.is(m.all[2], 'Ivysaur');
 	t.is(m.getName(143), 'Snorlax');
 	t.is(m.getId('Snorlax'), 143);
 });
