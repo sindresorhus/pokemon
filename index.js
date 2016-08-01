92e63f4073c0bdd9765a622ffef123f3a19caa42
Benny Neugebauer <bn@bennyn.de>
2016-08-01 17:49:58 +0200

    Asking for contribution

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function getLocalizedList(lang) {
 	try {
 		return require(`./pokemon_${lang}`);
 	} catch (err) {
-		throw new Error(`List for language code "${lang}" does not exist. ` +
+		throw new Error(`List for language code '${lang}' does not exist. ` +
 			`Please help to add more languages: https://github.com/${pkg.repository}`);
 	}
 }
@@ -24,7 +24,8 @@ exports.getName = (id, lang) => {
 	const name = list[id - 1];
 
 	if (!name) {
-		throw new Error(`Pokémon with ID ${id} does not exist`);
+		throw new Error(`Pokémon with ID '${id}' does not exist. ` +
+			`Please report to https://github.com/${pkg.repository}/issues if we missed something.`);
 	}
 
 	return name;
@@ -35,7 +36,8 @@ exports.getId = (name, lang) => {
 	const index = list.indexOf(name);
 
 	if (index === -1) {
-		throw new Error(`Pokémon with name ${name} does not exist`);
+		throw new Error(`Pokémon with name '${name}' does not exist for language code '${lang}'. ` +
+			`Please help to add more languages: https://github.com/${pkg.repository}`);
 	}
 
 	return index + 1;
