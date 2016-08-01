fce0d46da222eadb200016bd34ad8d59ace6bbc7
Benny Neugebauer <bn@bennyn.de>
2016-08-01 12:12:35 +0200

    Added German Pokémon names

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 'use strict';
 const uniqueRandomArray = require('unique-random-array');
 const pokemon = require('./pokemon');
+const pokemonGerman = require('./pokemon_de');
 
 exports.all = pokemon;
 exports.random = uniqueRandomArray(pokemon);
 
-exports.getName = id => {
-	const name = pokemon[id - 1];
+function getLocalizedPokemonList(lang) {
+	let list;
+
+	switch (lang) {
+		case 'de':
+			list = pokemonGerman;
+			break;
+		default:
+			list = pokemon;
+	}
+
+	return list;
+}
+
+exports.getName = (id, lang) => {
+	const list = getLocalizedPokemonList(lang);
+	const name = list[id - 1];
 
 	if (!name) {
 		throw new Error(`Pokémon with ID ${id} does not exist`);
@@ -15,8 +31,9 @@ exports.getName = id => {
 	return name;
 };
 
-exports.getId = name => {
-	const index = pokemon.indexOf(name);
+exports.getId = (name, lang) => {
+	const list = getLocalizedPokemonList(lang);
+	const index = list.indexOf(name);
 
 	if (index === -1) {
 		throw new Error(`Pokémon with name ${name} does not exist`);
--- test.js
+++ test.js
@@ -10,3 +10,45 @@ test(t => {
 	t.is(m.getName(143), 'Snorlax');
 	t.is(m.getId('Snorlax'), 143);
 });
+
+test('English names', t => {
+	t.is(m.getName(1), 'Bulbasaur');
+	t.is(m.getName(100), 'Voltorb');
+	t.is(m.getName(200), 'Misdreavus');
+	t.is(m.getName(300), 'Skitty');
+	t.is(m.getName(400), 'Bibarel');
+	t.is(m.getName(500), 'Emboar');
+	t.is(m.getName(600), 'Klang');
+	t.is(m.getName(700), 'Sylveon');
+	t.is(m.getName(721), 'Volcanion');
+});
+
+test('German names', t => {
+	t.is(m.getName(1, 'de'), 'Bisasam');
+	t.is(m.getName(100, 'de'), 'Voltobal');
+	t.is(m.getName(200, 'de'), 'Traunfugil');
+	t.is(m.getName(300, 'de'), 'Eneco');
+	t.is(m.getName(400, 'de'), 'Bidifas');
+	t.is(m.getName(500, 'de'), 'Flambirex');
+	t.is(m.getName(600, 'de'), 'Kliklak');
+	t.is(m.getName(700, 'de'), 'Feelinara');
+	t.is(m.getName(721, 'de'), 'Volcanion');
+});
+
+test('Indices can be used with language codes', t => {
+	t.is(m.getId('Snorlax'), 143);
+	t.is(m.getId('Relaxo', 'de'), 143);
+});
+
+test('Language fallback is English', t => {
+	const snorlaxId = 143;
+	const snorlaxEnglish = 'Snorlax';
+
+	t.is(m.getId(snorlaxEnglish), snorlaxId);
+	t.is(m.getId(snorlaxEnglish, 'en'), snorlaxId);
+	t.is(m.getId(snorlaxEnglish, 'invalid'), snorlaxId);
+
+	t.is(m.getName(snorlaxId), snorlaxEnglish);
+	t.is(m.getName(snorlaxId, 'en'), snorlaxEnglish);
+	t.is(m.getName(snorlaxId, 'invalid'), snorlaxEnglish);
+});
